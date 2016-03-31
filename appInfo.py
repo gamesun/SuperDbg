1925f42a554fd102b767b21769f33b0ee3da2572
sunyt <gamesunyt@gmail.com>
2016-03-31 11:56:05 +0800

    Create appInfo.py

---
+++ appInfo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+#############################################################################
+##
+## Copyright (c) 2016, gamesun
+## All right reserved.
+##
+## This file is part of SuperDbg.
+##
+## SuperDbg is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## SuperDbg is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with SuperDbg.  If not, see <http://www.gnu.org/licenses/>.
+##
+#############################################################################
+
+
+title = 'SuperDbg'
+version = '1.0.0'
+file_name = title + ' ' + version
+url = ''
+author = 'sun'
+copyright = 'Copyright (C) 2016, Gamesun'
+
